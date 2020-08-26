0c94b669fb402947a3064f6e8c2597147e459651
teekaytech <taofeek.olalere@feghas.com>
2020-08-26 11:01:19 +0100

    Add read toggle status, fix linter error

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const card = document.getElementById("card");
-const newBook = document.getElementById("new_book");
-const old_list = document.getElementById("list_book");
-const close = document.getElementById("close");
-const newBookForm = document.getElementById("form");
+const card = document.getElementById('card');
+const newBook = document.getElementById('new_book');
+const close = document.getElementById('close');
+const newBookForm = document.getElementById('form');
 
-let myLibrary = [];
+const myLibrary = [];
 
 function Book(author, title, pages, read = false) {
   this.id = null;
@@ -14,48 +13,77 @@ function Book(author, title, pages, read = false) {
   this.read = read;
 }
 
+/* eslint func-names: ["error", "never"] */
+Book.prototype.readStatus = function () {
+  if (this.read === true) {
+    this.read = false;
+  } else {
+    this.read = true;
+  }
+};
+
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+function displayBooks(library) {
+  card.innerHTML = '';
+  for (let i = 0; i < library.length; i += 1) {
+    DisplayBook(library[i]);
+  }
+}
+
+function deleteBook(e) {
+  const index = myLibrary.findIndex((x) => x.id === e.target.value);
+  myLibrary.splice(index, 1);
+  displayBooks(myLibrary);
+}
+
+function changeStatus(e) {
+  const index = myLibrary.findIndex((x) => x.id === Number(e.target.className));
+  myLibrary[index].readStatus();
+}
+
 function addBookToLibrary(book) {
   book.id = myLibrary.length;
   myLibrary.push(book);
 }
 
 function DisplayBook(book) {
-  let bookContainer = document.createElement("div");
+  const bookContainer = document.createElement('div');
   bookContainer.id = book.id;
-  bookContainer.className = "book_container";
+  bookContainer.className = 'book_container';
   card.appendChild(bookContainer);
 
-  let author = document.createElement("h3");
-  author.className = "author";
+  const author = document.createElement('h3');
+  author.className = 'author';
   author.innerHTML = `Author : ${book.author}`;
   bookContainer.appendChild(author);
 
-  let title = document.createElement("h3");
-  title.className = "title";
+  const title = document.createElement('h3');
+  title.className = 'title';
   title.innerHTML = `Title : ${book.title}`;
   bookContainer.appendChild(title);
 
-  let pages = document.createElement("h3");
-  pages.className = "pages";
+  const pages = document.createElement('h3');
+  pages.className = 'pages';
   pages.innerHTML = `Pages : ${book.pages}`;
   bookContainer.appendChild(pages);
 
-  let read = document.createElement("input");
-  read.className = "read";
-  read.value = "Read";
-  read.type = "checkbox";
+  const read = document.createElement('input');
+  read.className = book.id;
+  read.value = 'Read';
+  read.type = 'checkbox';
+  read.onchange = changeStatus;
   read.checked = book.read;
   bookContainer.appendChild(read);
 
-  let label = document.createElement("label");
-  label.className = "read_status";
-  label.innerHTML = "Read";
+  const label = document.createElement('label');
+  label.className = 'read_status';
+  label.innerHTML = 'Read';
   bookContainer.appendChild(label);
 
-  let removeButton = document.createElement("button");
-  removeButton.className = "removeBtn";
+  const removeButton = document.createElement('button');
+  removeButton.className = 'removeBtn';
   removeButton.value = book.id;
-  removeButton.innerHTML = "Remove";
+  removeButton.innerHTML = 'Remove';
 
   removeButton.onclick = deleteBook;
   bookContainer.appendChild(removeButton);
@@ -63,37 +91,24 @@ function DisplayBook(book) {
 
 displayBooks(myLibrary);
 
-newBook.addEventListener("click", function () {
-  newBookForm.style.display = "block";
+newBook.addEventListener('click', () => {
+  newBookForm.style.display = 'block';
 });
 
-close.addEventListener("click", function (e) {
+close.addEventListener('click', (e) => {
   e.preventDefault();
-  newBookForm.style.display = "none";
+  newBookForm.style.display = 'none';
 });
 
-function deleteBook(e) {
-  let index = myLibrary.findIndex((x) => x.id === e.target.value);
-  myLibrary.splice(index, 1);
-  displayBooks(myLibrary);
-}
-
-const createBook = document.getElementById("create_book");
-createBook.addEventListener("click", function (e) {
+const createBook = document.getElementById('create_book');
+createBook.addEventListener('click', (e) => {
   e.preventDefault();
-  let author = document.getElementById("author_name").value;
-  let title = document.getElementById("book_title").value;
-  let pages = document.getElementById("pages").value;
-  let read_status = document.getElementById("read_status").checked;
+  const author = document.getElementById('author_name').value;
+  const title = document.getElementById('book_title').value;
+  const pages = document.getElementById('pages').value;
+  const readStatus = document.getElementById('read_status').checked;
 
-  let newBook = new Book(author, title, pages, read_status);
+  const newBook = new Book(author, title, pages, readStatus);
   addBookToLibrary(newBook);
   displayBooks(myLibrary);
 });
-
-function displayBooks(library) {
-  card.innerHTML = "";
-  for (let i = 0; i < library.length; i++) {
-    DisplayBook(library[i]);
-  }
-}
